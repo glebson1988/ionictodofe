afaaa95e6bffa8dc6015e4dbb594cb4cee65e714
glebson1988 <glebson1988@yandex.ru>
2022-12-09 00:04:32 +0400

    ToDoListItem component with some styling plus handling events

--- src/components/ToDoListItem.tsx
+++ src/components/ToDoListItem.tsx
@@ -1,18 +1,25 @@
-import {IonButton, IonItem, IonLabel, IonNote, IonToggle} from "@ionic/react";
+import {IonButton, IonItem, IonLabel, IonToggle} from "@ionic/react";
 import {Todo} from "../data/todos";
 import './ToDoListItem.css';
 import React from "react";
 
 interface ToDoListItemProps {
-    todo: Todo,
+    todo: Todo;
     onToggleTodo: (todo: Todo) => void;
     onDeleteTodo: (todo: Todo) => void;
 }
 
-const ToDoListItem: React.FC<ToDoListItemProps> = ({todo, onToggleTodo,}) => {
+const ToDoListItem: React.FC<ToDoListItemProps> = ({todo, onToggleTodo, onDeleteTodo}) => {
     return (
         <IonItem>
-
+            <div slot='start' className={!todo.completed ? 'dot dot-unread' : 'dot'}></div>
+            <IonLabel>
+                <h2 className={todo.completed ? 'uncompleted' : 'completed'}>
+                    {todo.title}
+                </h2>
+            </IonLabel>
+            <IonToggle checked={todo.completed} onIonChange={() => onToggleTodo(todo)} color='primary'/>
+            <IonButton onClick={() => onDeleteTodo(todo)} color='danger'></IonButton>
         </IonItem>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,89 @@
 import MessageListItem from '../components/MessageListItem';
-import { useState } from 'react';
-import { Message, getMessages } from '../data/messages';
+import {useState} from 'react';
+import {Message, getMessages} from '../data/messages';
 import {
-  IonContent,
-  IonHeader,
-  IonList,
-  IonPage,
-  IonRefresher,
-  IonRefresherContent,
-  IonTitle,
-  IonToolbar,
-  useIonViewWillEnter
+    IonContent,
+    IonHeader,
+    IonList,
+    IonPage,
+    IonRefresher,
+    IonRefresherContent,
+    IonTitle,
+    IonToolbar,
+    useIonViewWillEnter
 } from '@ionic/react';
 import './Home.css';
+import {Todo, findAllTodos, findTodoById, createTodo, updateTodo, deleteTodo} from "../data/todos";
 
 const Home: React.FC = () => {
 
-  const [messages, setMessages] = useState<Message[]>([]);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
 
-  useIonViewWillEnter(() => {
-    const msgs = getMessages();
-    setMessages(msgs);
-  });
+    useIonViewWillEnter(() => {
+        const msgs = getMessages();
+        setMessages(msgs);
 
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
+        loadTodos();
+    });
 
-  return (
-    <IonPage id="home-page">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
+    const refresh = (e: CustomEvent) => {
+        setTimeout(() => {
+            e.detail.complete();
+        }, 3000);
+    };
 
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
+    const loadTodos = () => {
+        findAllTodos().then(res => {
+            setTodos(res.data);
+        });
+    };
 
-        <IonList>
-          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
+    const handleTodoCreate = (todo: Todo) => {
+        createTodo(todo).then(() => {
+            loadTodos();
+        });
+    };
+
+    const handleToggleTodoStatus = (todo: Todo) => {
+        todo.completed = !todo.completed
+        updateTodo(todo).then(() => {
+            loadTodos();
+        })
+    };
+
+    const handleTodoDelete = (todo: Todo) => {
+        deleteTodo(todo.id!).then(() => {
+            loadTodos();
+        });
+    };
+
+    return (
+        <IonPage id="home-page">
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Inbox</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonRefresher slot="fixed" onIonRefresh={refresh}>
+                    <IonRefresherContent></IonRefresherContent>
+                </IonRefresher>
+
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">
+                            Inbox
+                        </IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+
+                <IonList>
+                    {messages.map(m => <MessageListItem key={m.id} message={m}/>)}
+                </IonList>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
